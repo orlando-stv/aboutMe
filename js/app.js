dd037a538186e1a312e27200ded6cdf68402f7cb
Orlando v. F. <computronica31@gmail.com>
2024-06-07 11:38:40 -0500

    agregar while a js

--- js/app.js
+++ js/app.js
@@ -1,8 +1,7 @@
-'use strict';
+"use strict";
 
-const tu_nombre = prompt ("CUAL ES TU NOMBRE?") ;
-alert ("Genial " + tu_nombre + ", gracias por tomarte unos minutos");
-
+const tu_nombre = prompt("CUAL ES TU NOMBRE?");
+alert("Genial " + tu_nombre + ", gracias por tomarte unos minutos");
 
 const nombre = "si";
 const edad = "si";
@@ -12,49 +11,54 @@ const trabajos = "si";
 let contador_good = 0;
 let contador_mal = 0;
 
-const preg_nombre = prompt("¿Mi nombre completo es Orlando Stevens?");
-if (nombre.toLowerCase() === preg_nombre.toLowerCase()) {
-  alert("very good");
-  console.log ("very good");
-  contador_good += 1;
-} else {
-  alert("respuesta incorrecta");
-  console.log ("respuesta incorrecta");
-  contador_mal += 1;
-}
+let continuar;
+while (continuar != "no") {
+  const preg_nombre = prompt("¿Mi nombre completo es Orlando Stevens?");
+  if (nombre.toLowerCase() === preg_nombre.toLowerCase()) {
+    alert("very good");
+    console.log("very good");
+    contador_good += 1;
+  } else {
+    alert("respuesta incorrecta");
+    console.log("respuesta incorrecta");
+    contador_mal += 1;
+  }
 
-const preg_edad = prompt("¿mi edad es 36 años?");
+  const preg_edad = prompt("¿mi edad es 36 años?");
 
-if (edad.toLowerCase() === preg_edad.toLowerCase()) {
-  alert("very good");
-  console.log ("very good");
-  contador_good += 1;
-} else {
-  alert("respuesta incorrecta");
-  console.log ("respuesta incorrecta");
-  contador_mal += 1;
-}
-const preg_estudios = prompt("¿Estudie en Idat?");
-if (estudios.toLowerCase() === preg_estudios.toLowerCase()) {
-  alert("very good");
-  console.log ("very good");
-  contador_good += 1;
-} else {
-  alert("respuesta incorrecta");
-  console.log ("respuesta incorrecta");
-  contador_mal += 1;
-}
+  if (edad.toLowerCase() === preg_edad.toLowerCase()) {
+    alert("very good");
+    console.log("very good");
+    contador_good += 1;
+  } else {
+    alert("respuesta incorrecta");
+    console.log("respuesta incorrecta");
+    contador_mal += 1;
+  }
+  const preg_estudios = prompt("¿Estudie en Idat?");
+  if (estudios.toLowerCase() === preg_estudios.toLowerCase()) {
+    alert("very good");
+    console.log("very good");
+    contador_good += 1;
+  } else {
+    alert("respuesta incorrecta");
+    console.log("respuesta incorrecta");
+    contador_mal += 1;
+  }
 
-const preg_trabajos = prompt("Trabaje en CYBER 105?");
+  const preg_trabajos = prompt("Trabaje en CYBER 105?");
 
-if (trabajos.toLowerCase() === preg_trabajos.toLowerCase()) {
-  alert("very good");
-  console.log ("very good");
-  contador_good += 1;
-} else {
-  alert("respuesta incorrecta");
-  console.log ("respuesta incorrecta");
-  contador_mal += 1;
+  if (trabajos.toLowerCase() === preg_trabajos.toLowerCase()) {
+    alert("very good");
+    console.log("very good");
+    contador_good += 1;
+  } else {
+    alert("respuesta incorrecta");
+    console.log("respuesta incorrecta");
+    contador_mal += 1;
+  }
+
+  continuar = prompt("DESEA VOLVER A INTENTARLO?🤷‍♂️🐱‍👤");
 }
 
 alert(contador_good + " respuestas correctas");
