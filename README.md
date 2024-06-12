a78a29436184cd5ca80c608c96db573a142b59c1
Orlando v. F. <computronica31@gmail.com>
2024-06-12 12:09:41 -0500

    entregando lab03

--- README.md
+++ README.md
@@ -9,4 +9,14 @@ Contruí una web con un propóstito: Me conozcan mi procedencia, mi campo labora
 
 ### Autor: Orlando Villoslada Flores
 
-!["Resultado de mi accessibility](img/accessibility2.JPG){width=100px}
+!["Resultado de mi accessibility](./img/accessibility2.JPG){width=100px}
+
+# LABORATORIO 03
+
+## ABOUT-ME
+
+Contruí una web con un propóstito: Me conozcan mi procedencia, mi campo laboral, y mis proyecciones.
+
+### Autor: Orlando Villoslada Flores
+
+!["Resultado de mi accessibility](./img/accessibilityLab03){width=100px}
--- js/app.js
+++ js/app.js
@@ -12,71 +12,64 @@ const trabajos2 = "si";
 let contador_good = 0;
 let contador_mal = 0;
 
-let continuar;
-while (continuar != "no") {
-  const preg_nombre = prompt("¿Mi nombre completo es Orlando Stevens?");
-  if (nombre.toLowerCase() === preg_nombre.toLowerCase()) {
-    alert("very good");
-    console.log("very good");
-    contador_good += 1;
-  } else {
-    alert("respuesta incorrecta");
-    console.log("respuesta incorrecta");
-    contador_mal += 1;
-  }
-
-  const preg_edad = prompt("¿mi edad es 36 años?");
-
-  if (edad.toLowerCase() === preg_edad.toLowerCase()) {
-    alert("very good");
-    console.log("very good");
-    contador_good += 1;
-  } else {
-    alert("respuesta incorrecta");
-    console.log("respuesta incorrecta");
-    contador_mal += 1;
-  }
-  const preg_estudios = prompt("¿Estudie en Idat?");
-  if (estudios.toLowerCase() === preg_estudios.toLowerCase()) {
-    alert("very good");
-    console.log("very good");
-    contador_good += 1;
-  } else {
-    alert("respuesta incorrecta");
-    console.log("respuesta incorrecta");
-    contador_mal += 1;
-  }
-
-  const preg_trabajos = prompt("Trabaje en CYBER 105?");
-
-  if (trabajos.toLowerCase() === preg_trabajos.toLowerCase()) {
-    alert("very good");
-    console.log("very good");
-    contador_good += 1;
-  } else {
-    alert("respuesta incorrecta");
-    console.log("respuesta incorrecta");
-    contador_mal += 1;
-  }
-
-  const preg_trabajos2 = prompt("Trabaje en INVERSIONES LA CRUZ?");
-
-  if (trabajos2.toLowerCase() === preg_trabajos2.toLowerCase()) {
-    alert("very good");
-    console.log("very good");
-    contador_good += 1;
-  } else {
-    alert("respuesta incorrecta");
-    console.log("respuesta incorrecta");
-    contador_mal += 1;
-  }
-
-
-  continuar = prompt("DESEA VOLVER A INTENTARLO?🤷‍♂️🐱‍👤");
+const preg_nombre = prompt("¿Mi nombre completo es Orlando Stevens?");
+if (nombre.toLowerCase() === preg_nombre.toLowerCase()) {
+  alert("very good");
+  console.log("very good");
+  contador_good += 1;
+} else {
+  alert("respuesta incorrecta");
+  console.log("respuesta incorrecta");
+  contador_mal += 1;
 }
 
-alert(contador_good + " respuestas correctas");
-alert(contador_mal + " respuestas incorrectas");
+const preg_edad = prompt("¿mi edad es 36 años?");
+
+if (edad.toLowerCase() === preg_edad.toLowerCase()) {
+  alert("very good");
+  console.log("very good");
+  contador_good += 1;
+} else {
+  alert("respuesta incorrecta");
+  console.log("respuesta incorrecta");
+  contador_mal += 1;
+}
+const preg_estudios = prompt("¿Estudie en Idat?");
+if (estudios.toLowerCase() === preg_estudios.toLowerCase()) {
+  alert("very good");
+  console.log("very good");
+  contador_good += 1;
+} else {
+  alert("respuesta incorrecta");
+  console.log("respuesta incorrecta");
+  contador_mal += 1;
+}
+
+const preg_trabajos = prompt("Trabaje en CYBER 105?");
+
+if (trabajos.toLowerCase() === preg_trabajos.toLowerCase()) {
+  alert("very good");
+  console.log("very good");
+  contador_good += 1;
+} else {
+  alert("respuesta incorrecta");
+  console.log("respuesta incorrecta");
+  contador_mal += 1;
+}
+
+const preg_trabajos2 = prompt("Trabaje en INVERSIONES LA CRUZ?");
+
+if (trabajos2.toLowerCase() === preg_trabajos2.toLowerCase()) {
+  alert("very good");
+  console.log("very good");
+  contador_good += 1;
+} else {
+  alert("respuesta incorrecta");
+  console.log("respuesta incorrecta");
+  contador_mal += 1;
+}
+
+// continuamos con las preg 6 y 7 :-)
 
 alert("muchas gracias " + tu_nombre + ", sigamos adivinando");
 
@@ -87,6 +80,7 @@ while (cont_intento != 4) {
   const sexta_preg = Number(prompt("¿En que año naci?"));
   if (sexta_preg == 1987) {
     alert("very good");
+    contador_good += 1;
     cont_intento == 4;
     break;
   } else if (sexta_preg >= 1982 && sexta_preg <= 1992) {
@@ -96,29 +90,53 @@ while (cont_intento != 4) {
   }
   if (cont_intento == 4) {
     alert("Naci en 1987");
+    contador_mal += 1;
   }
 }
-
 
 let cont_array = 0;
 
 let condicion = false;
 
-const lista = ["Will Smith", "Dwayne Johnson", "Jim Carrey", "Johnny Depp", "Ryan Reynolds", "Kevin Costner", "Al Pacino", "Liam Neeson"];
+const lista = [
+  "Will Smith",
+  "Dwayne Johnson",
+  "Jim Carrey",
+  "Johnny Depp",
+  "Ryan Reynolds",
+  "Kevin Costner",
+  "Al Pacino",
+  "Liam Neeson",
+];
 while (cont_array != 6) {
   let actor = prompt("Adivina uno de mis actores recomendados").toLowerCase();
   for (let i = 0; i < lista.length; i++) {
     if (actor === lista[i].toLowerCase()) {
       condicion = true;
-
     }
   }
   if (condicion) {
     alert("excelente, ese actor es muy bueno");
+    contador_good += 1;
     break;
+  } else if (cont_array == 5) {
+    let salto_list = lista.join("\n");
+    alert(
+      "ese actor es bueno, pero no es mi recomendado, gracias por intentarlo ❤, Mis actores favoritos son: " +
+        "\n" +
+        salto_list
+    );
+    contador_mal += 1;
   } else {
-    alert("ese actor es bueno, pero no es mi recomendado");
+    alert(
+      "ese actor es bueno, pero no es mi recomendado, " +
+        "te quedan " +
+        (5 - cont_array) +
+        " intentos"
+    );
   }
-
   cont_array += 1;
 }
+
+alert(contador_good + " respuestas correctas");
+alert(contador_mal + " respuestas incorrectas");
