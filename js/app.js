a7a84544752c734e132875e82db46a166add3060
Orlando Stevens Villoslada Flores <computronica31@gmail.com>
2024-06-11 23:10:15 -0500

    en un 80%

--- js/app.js
+++ js/app.js
@@ -7,6 +7,7 @@ const nombre = "si";
 const edad = "si";
 const estudios = "si";
 const trabajos = "si";
+const trabajos2 = "si";
 
 let contador_good = 0;
 let contador_mal = 0;
@@ -58,8 +59,66 @@ while (continuar != "no") {
     contador_mal += 1;
   }
 
+  const preg_trabajos2 = prompt("Trabaje en INVERSIONES LA CRUZ?");
+
+  if (trabajos2.toLowerCase() === preg_trabajos2.toLowerCase()) {
+    alert("very good");
+    console.log("very good");
+    contador_good += 1;
+  } else {
+    alert("respuesta incorrecta");
+    console.log("respuesta incorrecta");
+    contador_mal += 1;
+  }
+
+
   continuar = prompt("DESEA VOLVER A INTENTARLO?🤷‍♂️🐱‍👤");
 }
 
 alert(contador_good + " respuestas correctas");
 alert(contador_mal + " respuestas incorrectas");
+
+alert("muchas gracias " + tu_nombre + ", sigamos adivinando");
+
+let cont_intento = 0;
+
+while (cont_intento != 4) {
+  cont_intento += 1;
+  const sexta_preg = Number(prompt("¿En que año naci?"));
+  if (sexta_preg == 1987) {
+    alert("very good");
+    cont_intento == 4;
+    break;
+  } else if (sexta_preg >= 1982 && sexta_preg <= 1992) {
+    alert("estas muy cerca, te quedan " + (4 - cont_intento) + " intentos");
+  } else {
+    alert("estas muy lejos, te quedan " + (4 - cont_intento) + " intentos");
+  }
+  if (cont_intento == 4) {
+    alert("Naci en 1987");
+  }
+}
+
+
+let cont_array = 0;
+
+let condicion = false;
+
+const lista = ["Will Smith", "Dwayne Johnson", "Jim Carrey", "Johnny Depp", "Ryan Reynolds", "Kevin Costner", "Al Pacino", "Liam Neeson"];
+while (cont_array != 6) {
+  let actor = prompt("Adivina uno de mis actores recomendados").toLowerCase();
+  for (let i = 0; i < lista.length; i++) {
+    if (actor === lista[i].toLowerCase()) {
+      condicion = true;
+
+    }
+  }
+  if (condicion) {
+    alert("excelente, ese actor es muy bueno");
+    break;
+  } else {
+    alert("ese actor es bueno, pero no es mi recomendado");
+  }
+
+  cont_array += 1;
+}
